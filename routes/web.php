9a92142f2cfb687780ed91e494c1c8d6a9c0c163
SheldonYoungo <sheldomyoungo@gmail.com>
2025-03-18 00:04:22 -0400

    Ya se puede publicar el fronted

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/quotes-ui', function () {
     return view('quotes');
 });
--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,46 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import autoprefixer from 'autoprefixer';
+import tailwindcss from '@tailwindcss/postcss';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        vue({
-            template: {
+  plugins: [
+      laravel({
+          input: ['resources/css/app.css', 'resources/js/app.js'],
+          refresh: true,
+      }),
+      vue({
+          template: {
               transformAssetUrls: {
-                base: null,
-                includeAbsolute: false,
+                  base: null,
+                  includeAbsolute: false,
               },
-            },
-          }),
-        tailwindcss(),
-    ],
-    resolve: {
-        alias: {
-          vue: 'vue/dist/vue.esm-bundler.js', // Asegúrate de usar esta versión
-        },
-    },
-    server: {
-      proxy: {
-        '/api': {
-          target: 'http://localhost:8000/api', // URL de tu servidor Laravel
-          changeOrigin: true,
-          secure: false,
-        },
+          },
+      }),
+  ],
+  resolve: {
+      alias: {
+          '@': path.resolve(__dirname, './resources/js'),
+          vue: 'vue/dist/vue.esm-bundler.js',
       },
-    },
+  },
+  css: {
+      postcss: {
+          plugins: [tailwindcss, autoprefixer],
+      },
+  },
+  build: {
+      outDir: "public/build",
+  },
+  server: {
+      proxy: {
+          '/api': {
+              target: 'http://localhost:8000/api',
+              changeOrigin: true,
+              secure: false,
+          },
+      },
+  },
 });
